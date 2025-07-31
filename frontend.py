b1d0b4f39d4869ba5e132826588e900939c132a5
harsha-andra <harsha.andra111@gmail.com>
2025-07-31 17:04:33 -0400

    frontend updated

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict"  # Change to your deployed URL when ready
+API_URL = "https://forcasting-ml-api.onrender.com"  # Change to your deployed URL when ready
 
 st.title("📈 Forecasting ML API Demo")
 
